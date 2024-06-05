b46d733db692b91164d984a7b80422458fee743c
CocaIsMee <anhphukareno@hphukarenogmail.arenogmailcom>
2024-06-05 09:57:59 +0700

    update BannerCarousel

--- src/components/BannerCarousel.tsx
+++ src/components/BannerCarousel.tsx
@@ -36,13 +36,13 @@ const ClientCarousel: React.FC = () => {
     };
 
     return (
-        <div className="testimonial p-5 bg-cover bg-center min-h-[200px]" style={{ backgroundImage: `url(../../images/testimonial.jpg)` }}>
-            <div className="container mx-auto p-5">
+        <div className="testimonial p-2 bg-cover bg-center min-h-[200px]" style={{ backgroundImage: `url(../../images/testimonial.jpg)` }}>
+            <div className="container mx-auto p-2">
                 <div className="relative">
                     {testimonials.map((testimonial, index) => (
                         <div key={index} className={`absolute inset-0 transition-opacity duration-1000 ${index === current ? 'opacity-100' : 'opacity-0'}`}>
-                            <div className="relative mb-4 mt-4 mx-auto w-[38rem] text-center">
-                                <img alt="" src={testimonial.img} className="img-fluid mb-3 rounded-full mx-auto" />
+                            <div className="relative mb-4 mt-4 mx-auto text-center" style={{ width: '100%' }}>
+                                <img alt="" src={testimonial.img} className="img-fluid mb-3 mx-auto w-full h-auto" />
                                 <h5 className="text-dark">{testimonial.name}</h5>
                                 <span className="text-secondary">{testimonial.title}</span>
                                 <p className="text-dark mt-3">{testimonial.text}</p>
@@ -50,9 +50,9 @@ const ClientCarousel: React.FC = () => {
                         </div>
                     ))}
                 </div>
-                <div className="flex justify-between mt-8">
-                    <button onClick={prevSlide} className="text-2xl">&#10094;</button>
-                    <button onClick={nextSlide} className="text-2xl">&#10095;</button>
+                <div className="flex justify-between mt-8 relative">
+                    <button onClick={prevSlide} className="absolute left-4 text-2xl bg-white rounded-full p-2">&#10094;</button>
+                    <button onClick={nextSlide} className="absolute right-4 text-2xl bg-white rounded-full p-2">&#10095;</button>
                 </div>
             </div>
         </div>
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -6,7 +6,7 @@ const Home = () => {
     return (
         <div>
             <BannerCarousel />
-            <h2 className='flex-1 flexStart gap-10' style={{ marginLeft: '50px', fontSize: '20px', marginTop: '400px' }}>Learners are viewing</h2>
+            <h2 className='flex-1 flexStart gap-10' style={{ marginLeft: '50px', fontSize: '20px', marginTop: '700px' }}>Learners are viewing</h2>
             <section className='flexStart paddings mb-16'>
                 <Categories />
             </section>
