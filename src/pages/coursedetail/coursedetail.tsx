ebe499f35225d890d7adbcfcefbf0c71b120c50c
CocaIsMee <anhphukareno@hphukarenogmail.arenogmailcom>
2024-06-13 16:04:45 +0700

    update course detail and fix popup categories

--- src/pages/coursedetail/coursedetail.tsx
+++ src/pages/coursedetail/coursedetail.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Collapse, Button } from 'antd';
 import { VideoCameraOutlined, PlayCircleOutlined, FileWordOutlined, QuestionCircleOutlined } from '@ant-design/icons';
 import { Link } from 'react-router-dom';
