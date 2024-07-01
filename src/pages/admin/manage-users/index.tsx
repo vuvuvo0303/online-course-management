6293f8291313d861088e973bbbeb6976aac69512
vuvuvo0303 <levanhoangvu2003@gmail.com>
2024-07-01 21:17:05 +0700

    merge

--- src/pages/admin/manage-users/index.tsx
+++ src/pages/admin/manage-users/index.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, useRef } from "react";
-import axios from "axios";
 import {
   Breadcrumb,
   Button,
@@ -9,99 +8,97 @@ import {
   Switch,
   Table,
   Modal,
-  Tag,
-  Popconfirm,
-  Radio,
-  Upload,
   Form,
+  Spin,
+  message,
+  Pagination,
+  Tag,
+  Upload,
+  Popconfirm,
 } from "antd";
-import {
-  DeleteOutlined,
-  EditOutlined,
-  HomeOutlined,
-  PlusOutlined,
-  SearchOutlined,
-  UserAddOutlined,
-} from "@ant-design/icons";
+import { DeleteOutlined, EditOutlined, HomeOutlined, PlusOutlined, SearchOutlined } from "@ant-design/icons";
 import { format } from "date-fns";
 import { Student } from "../../../models";
 import { toast } from "react-toastify";
 import Highlighter from "react-highlight-words";
-import type {
-  GetProp,
-  InputRef,
-  RadioChangeEvent,
-  TableColumnsType,
-  TableColumnType,
-  UploadFile,
-  UploadProps,
-} from "antd";
+import axiosInstance from "../../../services/api.ts";
+import type { InputRef, TableColumnsType, TableColumnType, UploadFile, UploadProps } from "antd";
 import type { FilterDropdownProps } from "antd/es/table/interface";
-import { host_main } from "../../../consts";
-import { useForm } from "antd/es/form/Form";
-
-type DataIndex = keyof Student;
+import { User } from "../../../models/User.ts";
+import uploadFile from "../../../utils/upload.ts";
 type FileType = Parameters<GetProp<UploadProps, "beforeUpload">>[0];
+
+type AxiosResponse = {
+  success: boolean;
+  data: any;
+  message?: string;
+  error?: [];
+};
+type DataIndex = keyof Student;
+
 const AdminManageUsers: React.FC = () => {
-  const [data, setData] = useState<Student[]>([]);
+  const [data, setData] = useState<User[]>([]);
   const [sortOrder, setSortOrder] = useState<{ [key: string]: "ascend" | "descend" }>({
-    createdDate: "ascend",
-    updatedDate: "ascend",
+    created_at: "ascend",
+    updated_at: "ascend",
   });
   const [searchText, setSearchText] = useState("");
-  const [searchedColumn, setSearchedColumn] = useState("");
+  const [searchedColumn, setSearchedColumn] = useState<DataIndex | "">("");
   const searchInput = useRef<InputRef>(null);
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [loading, setLoading] = useState<boolean>(false);
-  const [form] = useForm();
-  const token = localStorage.getItem("token");
-  const [value, setValue] = useState('Student');
+  const [form] = Form.useForm();
+  const [pagination, setPagination] = useState({ current: 1, pageSize: 10, total: 0 });
 
-  const getBase64 = (file: FileType): Promise<string> =>
-    new Promise((resolve, reject) => {
-      const reader = new FileReader();
-      reader.readAsDataURL(file);
-      reader.onload = () => resolve(reader.result as string);
-      reader.onerror = (error) => reject(error);
-    });
-  const [previewOpen, setPreviewOpen] = useState(false);
-  const [previewImage, setPreviewImage] = useState("");
-  const [fileList, setFileList] = useState<UploadFile[]>([]);
+  useEffect(() => {
+    fetchStudents();
+  }, [pagination.current, pagination.pageSize]);
 
-  const handlePreview = async (file: UploadFile) => {
-    if (!file.url && !file.preview) {
-      file.preview = await getBase64(file.originFileObj as FileType);
-    }
-
-    setPreviewImage(file.url || (file.preview as string));
-    setPreviewOpen(true);
-  };
-
-  const handleChange: UploadProps["onChange"] = ({ fileList: newFileList }) => setFileList(newFileList);
-
-  const uploadButton = (
-    <button style={{ border: 0, background: "none" }} type="button">
-      <PlusOutlined />
-      <div style={{ marginTop: 8 }}>Upload</div>
-    </button>
-  );
-  const handleDelete = async (_id: string, email: string) => {
+  const fetchStudents = async () => {
+    setLoading(true);
     try {
-      await axios.delete(`${host_main}/api/users/${_id}`, {
-        headers: {
-          Authorization: `Bearer ${token}`,
+      const response: AxiosResponse = await axiosInstance.post("/api/users/search", {
+        searchCondition: {
+          role: "all",
+          status: true,
+          is_delete: false,
+        },
+        pageInfo: {
+          pageNum: pagination.current,
+          pageSize: pagination.pageSize,
         },
       });
-      const listAfterDelete = data.filter((student) => student._id !== _id);
-      setData(listAfterDelete);
+
+      if (response.data && response.data.pageData) {
+        setData(response.data.pageData);
+        setPagination((prev) => ({
+          ...prev,
+          total: response.data.pageInfo.totalItems,
+          current: response.data.pageInfo.pageNum,
+          pageSize: response.data.pageInfo.pageSize,
+        }));
+      } else {
+        message.error("Failed to fetch students");
+      }
+    } catch (error) {
+      message.error("Error fetching students: " + error.message);
+    }
+    setLoading(false);
+  };
+
+  const handleDelete = async (_id: string, email: string) => {
+    try {
+      await axiosInstance.delete(`/api/users/${_id}`);
+      setData((prevData) => prevData.filter((student) => student._id !== _id));
       toast.success(`Deleted user ${email} successfully`);
+      fetchStudents();
     } catch (error) {
       toast.error(`Failed to delete user ${email}`);
     }
   };
+
   const addNewUser = async (values: Student) => {
     try {
-      // Check if email already exists
       const searchBody = {
         searchCondition: {
           keyword: values.email,
@@ -116,75 +113,38 @@ const AdminManageUsers: React.FC = () => {
       };
 
       setLoading(true);
-      const searchResponse = await axios.post(`${host_main}/api/users/search`, searchBody, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-          "Content-Type": "application/json",
-        },
-      });
+      console.log(values);
+      console.log(values.avatar.file.originFileObj);
+      const url = await uploadFile(values.avatar.file.originFileObj);
+      values.avatar = url;
+      const searchResponse = await axiosInstance.post(`/api/users/search`, searchBody);
 
-      if (searchResponse.data && searchResponse.data.data.pageData.length > 0) {
+      if (searchResponse.data && searchResponse.data.pageData.length > 0) {
         setLoading(false);
         return toast.error("Email already exists in the database.");
       }
 
-      // Add new user if email does not exist
-      const response = await axios.post(`${host_main}/api/users/create`, values, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-
-      if (response.data.status === false) {
-        return toast.error(response.data.message);
+      const response: AxiosResponse = await axiosInstance.post(`/api/users/create`, values);
+      if (response.success === false) {
+        setLoading(false);
+        return toast.error(response.message);
       }
-
-      setData([...data, response.data.data]);
-      toast.success("Create new student successfully");
+      const newUser = response.data;
+      setData((prevData) => [...prevData, newUser]);
+      toast.success("Created new student successfully");
       setIsModalVisible(false);
       form.resetFields();
       setLoading(false);
+      fetchStudents();
     } catch (error) {
-      toast.error("Failed to add user: " + error);
+      toast.error("Failed to add user: " + (error as any).message);
+      setLoading(false);
     }
-    setIsModalVisible(false);
   };
-
-  useEffect(() => {
-    const fetchStudents = async () => {
-      try {
-        const searchBody = {
-          searchCondition: {
-            keyword: "",
-            role: "all",
-            status: true,
-            is_delete: false,
-          },
-          pageInfo: {
-            pageNum: 1,
-            pageSize: 10,
-          },
-        };
-
-        const response = await axios.post(`${host_main}/api/users/search`, searchBody, {
-          headers: {
-            Authorization: `Bearer ${token}`,
-            "Content-Type": "application/json",
-          },
-        });
-
-        setData(response.data.data.pageData);
-      } catch (error) {
-        console.error("Error fetching students:", error);
-      }
-    };
-
-    fetchStudents();
-  }, [token]);
 
   const formatDate = (dateString: string) => {
     try {
-      return format(new Date(dateString), "dd/MM/yyyy");
+      return format(new Date(dateString), "dd/MM/yyyy HH:mm:ss");
     } catch (error) {
       console.error("Invalid date:", dateString);
       return "Invalid date";
@@ -225,7 +185,35 @@ const AdminManageUsers: React.FC = () => {
     clearFilters();
     setSearchText("");
   };
+  const getBase64 = (file: FileType): Promise<string> =>
+    new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => resolve(reader.result as string);
+      reader.onerror = (error) => reject(error);
+    });
 
+  const [previewOpen, setPreviewOpen] = useState(false);
+  const [previewImage, setPreviewImage] = useState("");
+  const [fileList, setFileList] = useState<UploadFile[]>([]);
+
+  const handlePreview = async (file: UploadFile) => {
+    if (!file.url && !file.preview) {
+      file.preview = await getBase64(file.originFileObj as FileType);
+    }
+
+    setPreviewImage(file.url || (file.preview as string));
+    setPreviewOpen(true);
+  };
+
+  const handleChange: UploadProps["onChange"] = ({ fileList: newFileList }) => setFileList(newFileList);
+
+  const uploadButton = (
+    <button style={{ border: 0, background: "none" }} type="button">
+      <PlusOutlined />
+      <div style={{ marginTop: 8 }}>Upload</div>
+    </button>
+  );
   const getColumnSearchProps = (dataIndex: DataIndex): TableColumnType<Student> => ({
     filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters, close }) => (
       <div style={{ padding: 8 }} onKeyDown={(e) => e.stopPropagation()}>
@@ -302,44 +290,43 @@ const AdminManageUsers: React.FC = () => {
       title: "Name",
       dataIndex: "name",
       key: "name",
-      width: "20%",
       ...getColumnSearchProps("name"),
+      sorter: true,
+      sortDirections: ["descend", "ascend"],
+      onHeaderCell: () => ({
+        onClick: () => sortColumn("name"),
+      }),
     },
     {
       title: "Email",
       dataIndex: "email",
       key: "email",
-      width: "20%",
       ...getColumnSearchProps("email"),
+      sorter: true,
+      sortDirections: ["descend", "ascend"],
+      onHeaderCell: () => ({
+        onClick: () => sortColumn("email"),
+      }),
     },
     {
       title: "Role",
       dataIndex: "role",
       key: "role",
-      width: "5%",
-
-      render: (role) => {
-        if (role === "student") {
-          return <Tag color="geekblue">Student</Tag>;
-        } else if (role === "instructor") {
-          return <Tag color="gold">Instructor</Tag>;
-        }
-        return null;
-      },
+      render: (role) => (
+        <Tag color={role === "student" ? "cyan" : role === "instructor" ? "lime" : "default"}>{role}</Tag>
+      ),
     },
-
     {
       title: "Created Date",
       dataIndex: "created_at",
-      key: "create_at",
+      key: "created_at",
       render: (created_at: string) => formatDate(created_at),
       sorter: true,
-
       sortDirections: ["descend", "ascend"],
       onHeaderCell: () => ({
         onClick: () => sortColumn("created_at"),
       }),
-      width: "13%",
+      width: "15%",
     },
     {
       title: "Updated Date",
@@ -351,10 +338,10 @@ const AdminManageUsers: React.FC = () => {
       onHeaderCell: () => ({
         onClick: () => sortColumn("updated_at"),
       }),
-      width: "13%",
+      width: "15%",
     },
     {
-      title: "Avatar",
+      title: "Image",
       dataIndex: "avatar",
       key: "avatar",
       render: (avatar: string) => <Image src={avatar} width={50} />,
@@ -375,11 +362,11 @@ const AdminManageUsers: React.FC = () => {
         <div>
           <EditOutlined className="hover:cursor-pointer text-blue-400 hover:opacity-60" style={{ fontSize: "20px" }} />
           <Popconfirm
-            title="Delete the task"
-            description="Are you sure to delete this task?"
+            title="Delete the User"
+            description="Are you sure to delete this User?"
+            onConfirm={() => handleDelete(record._id, record.email)}
             okText="Yes"
             cancelText="No"
-            onConfirm={() => handleDelete(record._id, record.email)}
           >
             <DeleteOutlined
               className="ml-5 text-red-500 hover:cursor-pointer hover:opacity-60"
@@ -390,9 +377,13 @@ const AdminManageUsers: React.FC = () => {
       ),
     },
   ];
-  const onChange = (e: RadioChangeEvent) => {
-    console.log("radio checked", e.target.value);
-    setValue(e.target.value);
+
+  const handleTableChange = (pagination: any) => {
+    setPagination(pagination);
+  };
+
+  const handlePaginationChange = (page: number, pageSize: number) => {
+    setPagination({ ...pagination, current: page, pageSize });
   };
 
   return (
@@ -405,12 +396,30 @@ const AdminManageUsers: React.FC = () => {
           <Breadcrumb.Item>Admin</Breadcrumb.Item>
           <Breadcrumb.Item>Manage Students</Breadcrumb.Item>
         </Breadcrumb>
-        <Button type="primary" onClick={() => setIsModalVisible(true)} className="py-4">
-          <UserAddOutlined />
+        <Button type="primary" onClick={() => setIsModalVisible(true)}>
           Add New Student
         </Button>
       </div>
-      <Table columns={columns} dataSource={data} rowKey="_id" />
+      <Spin spinning={loading}>
+        <Table
+          columns={columns}
+          dataSource={data}
+          rowKey="_id"
+          pagination={false} // Disable the default pagination
+          onChange={handleTableChange}
+        />
+      </Spin>
+      <div className="flex justify-end py-8">
+        <Pagination
+          total={pagination.total}
+          showTotal={(total) => `Total ${total} items`}
+          current={pagination.current}
+          pageSize={pagination.pageSize}
+          onChange={handlePaginationChange}
+          showSizeChanger
+        />
+      </div>
+
       <Modal title="Add New Student" visible={isModalVisible} onCancel={() => setIsModalVisible(false)} footer={null}>
         <Form form={form} onFinish={addNewUser} labelCol={{ span: "24" }}>
           <Form.Item label="Name" name="name" rules={[{ required: true, message: "Please input the name!" }]}>
@@ -426,13 +435,7 @@ const AdminManageUsers: React.FC = () => {
           >
             <Input.Password />
           </Form.Item>
-          <Form.Item label="Role" name="role" rules={[{ required: true, message: "Please choose the role!" }]}>
-            <Radio.Group onChange={onChange} value={value}>
-              <Radio value="Student">Student</Radio>
-              <Radio value="Instructor">Instructor</Radio>
-            </Radio.Group>
-          </Form.Item>
-          <Form.Item label="Avatar" name="avatarUrl" rules={[{ required: true, message: "Please upload your Avatar" }]}>
+          <Form.Item label="Avatar" name="avatar" rules={[{ required: true, message: "Please upload your Avatar" }]}>
             <Upload
               action="https://660d2bd96ddfa2943b33731c.mockapi.io/api/upload"
               listType="picture-card"
