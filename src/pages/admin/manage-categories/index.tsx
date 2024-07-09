495d5178d817982f34dbe9ab7e8da3e525083528
duongtoan106 <phamduongtoan106@gmail.com>
2024-07-10 06:15:58 +0700

    apply callback in category manage

--- src/pages/admin/manage-categories/index.tsx
+++ src/pages/admin/manage-categories/index.tsx
@@ -1,10 +1,4 @@
-import React, {
-  useState,
-  useEffect,
-  useRef,
-  useCallback,
-  RefObject,
-} from "react";
+import React, { useState, useEffect, useRef, useCallback } from "react";
 import {
   Breadcrumb,
   Button,
