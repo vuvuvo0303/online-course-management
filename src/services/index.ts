d1b67a556ed502cdaa818e42b361bd23f1d58ab9
TanHuynh68 <tanhpce171112@fpt.edu.vn>
2024-07-24 23:10:56 +0700

    api services of coruse, session, lession

--- src/services/index.ts
+++ src/services/index.ts
@@ -4,3 +4,6 @@ export { getItemsByStudent, getItemsByInstructor } from './purchase';
 export { getInstructorDetailPublic } from './users';
 export { handleSubscriptionByInstructorOrStudent, getItemsBySubscriber } from './subscription';
 export { createPayout, getPayouts, updateStatusPayout } from './payout';
+export { createCourseByInstructor, getCourses, getCourse, updateCourseByInstructor, deleteCourse, changeStatusCourse, getCourseLogs } from './course';
+export { createSession, getSessions, getSession, updateSession, deleteSession } from './session';
+export { createLesson, getLessons, getLesson, updateLesson, deleteLesson } from './lesson';
