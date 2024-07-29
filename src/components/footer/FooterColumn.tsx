e85647c8e553fc2a55eed68a540542fbf1330568
duongtoan106 <phamduongtoan106@gmail.com>
2024-07-29 09:00:34 +0700

    edit layout cart & footer

--- src/components/footer/FooterColumn.tsx
+++ src/components/footer/FooterColumn.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 type FooterLink = {
   name: string;
@@ -11,12 +11,15 @@ type FooterColumnProps = {
 };
 
 const FooterColumn: React.FC<FooterColumnProps> = ({ title, links }) => (
-  <div className="flex flex-col w-full md:flex-col">
+  <div className="flex flex-col w-full md:flex-col mr-5">
     <h3 className="font-semibold text-lg mb-4">{title}</h3>
     <ul className="list-none mt-4">
       {links.map((link, index) => (
         <li key={index} className="mb-2">
-          <a href={link.url} className="text-base font-medium text-gray-600 hover:underline">
+          <a
+            href={link.url}
+            className="text-base font-medium text-gray-600 hover:underline"
+          >
             {link.name}
           </a>
         </li>
