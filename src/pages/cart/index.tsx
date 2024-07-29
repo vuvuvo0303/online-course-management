b840f1d2437b1564071aae7204c6a04b980f36f5
duongtoan106 <phamduongtoan106@gmail.com>
2024-07-29 07:08:36 +0700

    edit layout for cart

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,13 +1,13 @@
-import styles from '../../components/cart/cartComponents.module.css';
+import styles from "../../components/cart/cartComponents.module.css";
 import { CartComponents } from "../../components";
-import { deleteCart, getCarts, updateStatusCart } from '../../services/cart.ts';
-import { useEffect, useState } from 'react';
-import { Cart } from '../../models';
-import { useNavigate } from 'react-router-dom';
+import { deleteCart, getCarts, updateStatusCart } from "../../services/cart.ts";
+import { useEffect, useState } from "react";
+import { Cart } from "../../models";
+import { useNavigate } from "react-router-dom";
 import { Col, message, Row } from "antd";
 import { paths } from "../../consts";
-import { Link } from 'react-router-dom';
-import CustomButton from '../../components/CustomButton.tsx';
+import { Link } from "react-router-dom";
+import CustomButton from "../../components/CustomButton.tsx";
 import LoadingComponent from "../../components/loading";
 const CartPage: React.FC = () => {
   const [cartsNew, setCartsNew] = useState<Cart[]>([]);
@@ -40,13 +40,13 @@ const CartPage: React.FC = () => {
   const token = localStorage.getItem("token");
 
   const getCartWaitingPaid = async () => {
-    setLoading(true)
+    setLoading(true);
     const res = await getCarts("waiting_paid");
     if (res) {
       setCartsWaitingPaid(res);
-      setLoading(false)
+      setLoading(false);
     }
-  }
+  };
 
   useEffect(() => {
     if (token) {
@@ -57,9 +57,11 @@ const CartPage: React.FC = () => {
   }, [token]);
 
   if (loading) {
-    return (<>
-      <LoadingComponent />
-    </>)
+    return (
+      <>
+        <LoadingComponent />
+      </>
+    );
   }
 
   const handleDeleteCart = async (cart_id: string) => {
@@ -67,7 +69,6 @@ const CartPage: React.FC = () => {
     getCartNew();
     getCartCancel();
   };
-
 
   // handle calculate total cost, money for paid and total course
   const handlePlus = (cart: Cart[]) => {
@@ -84,10 +85,10 @@ const CartPage: React.FC = () => {
 
   const onChangeCheckBox = (cart: Cart) => {
     let index = indexCartChecked;
-    let foundCartId = cartsChecked.find(cartId => cartId === cart);
+    let foundCartId = cartsChecked.find((cartId) => cartId === cart);
     let newArray: Cart[] = [];
     if (foundCartId) {
-      newArray = cartsChecked.filter(item => item !== foundCartId);
+      newArray = cartsChecked.filter((item) => item !== foundCartId);
       index--;
       setIndexCartChecked(index);
     } else {
@@ -125,7 +126,7 @@ const CartPage: React.FC = () => {
       if (cartsWaitingPaid.length > 0) {
         navigate(paths.STUDENT_CHECKOUT);
       } else {
-        message.error("Please select at least one cart")
+        message.error("Please select at least one cart");
       }
     }
   };
@@ -133,36 +134,51 @@ const CartPage: React.FC = () => {
   return (
     <div className="py-0 md:px-[4.8rem] px-4 mb-[4.4rem] max-w-[134rem] my-0 mx-auto">
       <h1 className="mt-10 main_h1">Course Cart</h1>
-      <h3 className={`${styles.h3_cart_title} mt-10`}>{totalCourse} Courses in Cart</h3>
-      <div className="mt-8">
-        {
-          (cartsCancel.length > 0 || cartsNew.length >0) &&
-          <Row className='border p-5 mt-10' gutter={10}>
-          <Col className='font-bold text-center' span={6}>Course</Col>
-          <Col span={6}></Col>
-          <Col className='font-bold' span={6}>Discount</Col>
-          <Col className='font-bold' span={6}>
-            <Row>
-              <Col span={12}>Total</Col>
-              <Col span={12}>Action</Col>
+      <h3 className={`${styles.h3_cart_title} mt-10`}>
+        {totalCourse} Courses in Cart
+      </h3>
+      <div className="flex mt-8">
+        <div className="flex-6 pr-4">
+          {(cartsCancel.length > 0 || cartsNew.length > 0) && (
+            <Row className="border p-5 mt-10" gutter={10}>
+              <Col className="font-bold text-center" span={6}>
+                Course
+              </Col>
+              <Col span={6}></Col>
+              <Col className="font-bold" span={6}>
+                Discount
+              </Col>
+              <Col className="font-bold" span={6}>
+                <Row>
+                  <Col span={12}>Total</Col>
+                  <Col span={12}>Action</Col>
+                </Row>
+              </Col>
             </Row>
-          </Col>
-        </Row>
-        }
-        <div className=" mt-0">
-          <div>
-            <ul className="min-w-full m-0 p-0">
-              {(cartsNew.length === 0 && cartsCancel.length === 0) &&
-                <div className={styles.empty_cart_container}>
-                  <img width={200} height={200} alt='empty-cart-display' src='https://s.udemycdn.com/browse_components/flyout/empty-shopping-cart-v2-2x.jpg' />
-                  <p className='text-lg mb-4'>Your cart is empty. Keep shopping to find a course!</p>
-                  <Link to={paths.HOME}>
-                    <CustomButton title='Keep Shopping' containerStyles='bg-purple-500' />
-                  </Link>
-                </div>
-              }
-              {
-                cartsNew.map((cart) =>
+          )}
+          <div className="mt-0">
+            <div>
+              <ul className="min-w-full m-0 p-0">
+                {cartsNew.length === 0 && cartsCancel.length === 0 && (
+                  <div className={styles.empty_cart_container}>
+                    <img
+                      width={200}
+                      height={200}
+                      alt="empty-cart-display"
+                      src="https://s.udemycdn.com/browse_components/flyout/empty-shopping-cart-v2-2x.jpg"
+                    />
+                    <p className="text-lg mb-4">
+                      Your cart is empty. Keep shopping to find a course!
+                    </p>
+                    <Link to={paths.HOME}>
+                      <CustomButton
+                        title="Keep Shopping"
+                        containerStyles="bg-purple-500"
+                      />
+                    </Link>
+                  </div>
+                )}
+                {cartsNew.map((cart) => (
                   <CartComponents
                     cartsNew={cart}
                     totalCost={totalCost}
@@ -171,10 +187,8 @@ const CartPage: React.FC = () => {
                     handleDeleteCart={() => handleDeleteCart(cart._id)}
                     cartsWaitingPaid={cartsWaitingPaid}
                   />
-                )
-              }
-              {
-                cartsCancel.map((cart) =>
+                ))}
+                {cartsCancel.map((cart) => (
                   <CartComponents
                     cartsNew={cart}
                     totalCost={totalCost}
@@ -183,49 +197,60 @@ const CartPage: React.FC = () => {
                     handleDeleteCart={() => handleDeleteCart(cart._id)}
                     cartsWaitingPaid={cartsWaitingPaid}
                   />
-                )
-              }
-            </ul>
-            {(cartsNew.length > 0 || cartsCancel.length > 0) &&
-              <div className={styles.cart_checkout}>
-                <div className={styles.total_price}>
-                  <div className={styles.total_checkout_container}>
-                    <div className={styles.total_price_label}>Total:</div>
-                    <div className={styles.base_price_heading}>
-                      <span>₫{totalMoney}</span>
-                    </div>
-                    <div className={styles.discount_price_checkout}>
-                      <div>
-                        <s>
-                          <span>₫{totalCost}</span>
-                        </s>
-                      </div>
-                    </div>
-                    <div className={styles.discount_price_checkout}>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="flex-4">
+          {(cartsNew.length > 0 || cartsCancel.length > 0) && (
+            <div className={styles.cart_checkout}>
+              <div className={styles.total_price}>
+                <div className={styles.total_checkout_container}>
+                  <div className={styles.total_price_label}>Total:</div>
+                  <div className={styles.base_price_heading}>
+                    <span>₫{totalMoney}</span>
+                  </div>
+                  <div className={styles.discount_price_checkout}>
+                    <div>
+                      <s>
+                        <span>₫{totalCost}</span>
+                      </s>
                     </div>
                   </div>
-                  {
-                    cartsWaitingPaid.length > 0 ? (
-                      cartsChecked.length > 0 ? (
-                        <button onClick={handleCheckoutNow} className={styles.checkout_button}>
-                          Checkout now with {cartsWaitingPaid.length} art in checkout page and {cartsChecked.length} cart checked
-                        </button>
-                      ) : (
-                        <>
-                          <button onClick={handleCheckoutNow} className={styles.checkout_button}>
-                            Checkout now with {cartsWaitingPaid.length} cart in checkout page
-                          </button>
-                        </>
-                      )
-
-                    ) : (
-                      <button onClick={handleCheckoutNow} className={styles.checkout_button}>Checkout now</button>
-                    )
-                  }
+                  <div className={styles.discount_price_checkout}></div>
                 </div>
+                {cartsWaitingPaid.length > 0 ? (
+                  cartsChecked.length > 0 ? (
+                    <button
+                      onClick={handleCheckoutNow}
+                      className={styles.checkout_button}
+                    >
+                      Checkout now with {cartsWaitingPaid.length} art in
+                      checkout page and {cartsChecked.length} cart checked
+                    </button>
+                  ) : (
+                    <>
+                      <button
+                        onClick={handleCheckoutNow}
+                        className={styles.checkout_button}
+                      >
+                        Checkout now with {cartsWaitingPaid.length} cart in
+                        checkout page
+                      </button>
+                    </>
+                  )
+                ) : (
+                  <button
+                    onClick={handleCheckoutNow}
+                    className={styles.checkout_button}
+                  >
+                    Checkout now
+                  </button>
+                )}
               </div>
-            }
-          </div>
+            </div>
+          )}
         </div>
       </div>
     </div>
