c9fece353bb1676857269dd186bdd4d181b7f48e
duongtoan106 <phamduongtoan106@gmail.com>
2024-08-02 20:41:14 +0700

    add button on login and register page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,12 +1,22 @@
 import { useState } from "react";
-import { Form, FormProps, Input, Modal, Select } from "antd";
+import { Button, Form, FormProps, Input, Modal, Select } from "antd";
 import { Link, useNavigate } from "react-router-dom";
-import Login5 from "../../assets/Login5.jpg";
+import Login4 from "../../assets/Login4.jpg";
 import { paths, roles } from "../../consts";
 import { GoogleLogin } from "@react-oauth/google";
-import { handleNavigateRole, login, loginWithGoogle, registerWithGoogle } from "../../services";
-import { EmailFormItem, LoginButtonItem, PasswordFormItem } from "../../components";
+import {
+  handleNavigateRole,
+  login,
+  loginWithGoogle,
+  registerWithGoogle,
+} from "../../services";
+import {
+  EmailFormItem,
+  LoginButtonItem,
+  PasswordFormItem,
+} from "../../components";
 import { LoginFieldType } from "../../models/Auth";
+import { LeftOutlined } from "@ant-design/icons";
 
 const LoginPage: React.FC = () => {
   const navigate = useNavigate();
@@ -31,7 +41,9 @@ const LoginPage: React.FC = () => {
     setLoading(false);
   };
 
-  const handleAdditionalFieldsChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleAdditionalFieldsChange = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
     const { name, value } = e.target;
     setAdditionalFields((prevFields) => ({
       ...prevFields,
@@ -59,22 +71,43 @@ const LoginPage: React.FC = () => {
   const renderGoogleLogin = () => (
     <GoogleLogin
       onSuccess={(credentialResponse) => {
-        loginWithGoogle(credentialResponse.credential as string, navigate, setIsModalVisible);
+        loginWithGoogle(
+          credentialResponse.credential as string,
+          navigate,
+          setIsModalVisible
+        );
         localStorage.setItem("token", credentialResponse.credential as string);
       }}
     />
   );
 
   return (
-    <div className="min-h-screen flex justify-center items-center bg-gradient-to-b from-[#18a5a7] via-[#ffe998] to-[#ffb330] relative">
+    <div className="min-h-screen flex justify-center items-center bg-gradient-to-b from-[#fffcce] to-[#1e5b53] relative">
+      <Button
+        type="link"
+        icon={<LeftOutlined />}
+        onClick={() => navigate(paths.HOME)}
+        className="absolute top-4 left-4 text-blue-500 bg-white bg-opacity-70 font-bold py-2 px-4 rounded-lg inline-flex items-center"
+      >
+        Home
+      </Button>
+
       <div className="w-full md:w-1/2 flex flex-row bg-white rounded-lg shadow-lg overflow-hidden min-h-[650px] mb-[30px]">
         <div className="w-1/2 flex items-center justify-center">
-          <img src={Login5} alt="Vector" className="object-cover w-full h-full" />
+          <img
+            src={Login4}
+            alt="Vector"
+            className="object-cover w-full h-full"
+          />
         </div>
         <div className="w-1/2 flex flex-col justify-center p-4 md:p-8 bg-white rounded-lg">
           <div className="flex flex-col items-center mb-4">
-            <h1 className="mb-2 text-2xl md:text-3xl font-bold text-center">Welcome</h1>
-            <span className="text-sm md:text-base text-center">Log in to become a part of FLearn</span>
+            <h1 className="mb-2 text-2xl md:text-3xl font-bold text-center">
+              Welcome
+            </h1>
+            <span className="text-sm md:text-base text-center">
+              Log in to become a part of FLearn
+            </span>
           </div>
           <Form
             name="basic"
@@ -86,7 +119,10 @@ const LoginPage: React.FC = () => {
             <EmailFormItem />
             <PasswordFormItem />
             <div className="flex justify-center">
-              <Link className="hover:text-blue-600 mt-2" to={paths.FORGOT_PASSWORD}>
+              <Link
+                className="hover:text-blue-600 mt-2"
+                to={paths.FORGOT_PASSWORD}
+              >
                 Forgot Password
               </Link>
             </div>
@@ -95,7 +131,10 @@ const LoginPage: React.FC = () => {
           <span className="mt-4 block text-center">
             Do you have an account?{" "}
             <strong>
-              <Link to={paths.REGISTER} className="hover:cursor-pointer hover:text-blue-600">
+              <Link
+                to={paths.REGISTER}
+                className="hover:cursor-pointer hover:text-blue-600"
+              >
                 Sign up here
               </Link>
             </strong>
@@ -108,9 +147,19 @@ const LoginPage: React.FC = () => {
           <div className="flex justify-center mt-6">{renderGoogleLogin()}</div>
         </div>
       </div>
-      <Modal title="Select Role" open={isModalVisible} onOk={handleModalOk} onCancel={handleModalCancel}>
+      <Modal
+        title="Select Role"
+        open={isModalVisible}
+        onOk={handleModalOk}
+        onCancel={handleModalCancel}
+      >
         <Form>
-          <Form.Item label="Role" required labelCol={{ span: 24 }} wrapperCol={{ span: 24 }}>
+          <Form.Item
+            label="Role"
+            required
+            labelCol={{ span: 24 }}
+            wrapperCol={{ span: 24 }}
+          >
             <Select onChange={handleRoleChange}>
               <Select.Option value="student">Student</Select.Option>
               <Select.Option value="instructor">Instructor</Select.Option>
@@ -118,7 +167,12 @@ const LoginPage: React.FC = () => {
           </Form.Item>
           {role === roles.INSTRUCTOR && (
             <>
-              <Form.Item label="Description" required labelCol={{ span: 24 }} wrapperCol={{ span: 24 }}>
+              <Form.Item
+                label="Description"
+                required
+                labelCol={{ span: 24 }}
+                wrapperCol={{ span: 24 }}
+              >
                 <Input
                   placeholder="Description"
                   name="description"
@@ -126,7 +180,12 @@ const LoginPage: React.FC = () => {
                   onChange={handleAdditionalFieldsChange}
                 />
               </Form.Item>
-              <Form.Item label="Phone Number" required labelCol={{ span: 24 }} wrapperCol={{ span: 24 }}>
+              <Form.Item
+                label="Phone Number"
+                required
+                labelCol={{ span: 24 }}
+                wrapperCol={{ span: 24 }}
+              >
                 <Input
                   placeholder="Phone Number"
                   name="phone_number"
@@ -134,8 +193,17 @@ const LoginPage: React.FC = () => {
                   onChange={handleAdditionalFieldsChange}
                 />
               </Form.Item>
-              <Form.Item label="Video URL" required labelCol={{ span: 24 }} wrapperCol={{ span: 24 }}>
-                <Input name="video" value={additionalFields.video} onChange={handleAdditionalFieldsChange} />
+              <Form.Item
+                label="Video URL"
+                required
+                labelCol={{ span: 24 }}
+                wrapperCol={{ span: 24 }}
+              >
+                <Input
+                  name="video"
+                  value={additionalFields.video}
+                  onChange={handleAdditionalFieldsChange}
+                />
               </Form.Item>
             </>
           )}
