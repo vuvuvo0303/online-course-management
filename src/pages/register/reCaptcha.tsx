cf40270ad9eaf4e732ee2e59fb919f8f0994e916
Duc-dev-starter <leminhduck@gmail.com>
2024-08-05 09:52:18 +0700

    fix recaptcha key

--- src/pages/register/reCaptcha.tsx
+++ src/pages/register/reCaptcha.tsx
@@ -1,20 +1,12 @@
-// ReCaptcha.tsx
-import React from "react";
 import ReCAPTCHA from "react-google-recaptcha";
+import config from "../../secret/config";
 
-const Recaptcha: React.FC<{ onVerify: (token: string | null) => void }> = ({
-  onVerify,
-}) => {
+const Recaptcha: React.FC<{ onVerify: (token: string | null) => void }> = ({ onVerify }) => {
   const handleVerify = (token: string | null) => {
     onVerify(token);
   };
 
-  return (
-    <ReCAPTCHA
-      sitekey="6LcrkgkqAAAAANcrH9wjWrfV9TMzigiNB32SNBiV"
-      onChange={handleVerify}
-    />
-  );
+  return <ReCAPTCHA sitekey={config.RECAPTCHA_KEY} onChange={handleVerify} />;
 };
 
 export default Recaptcha;
