507ffd983a9ec22a9d95a7e82826307e6e9ce087
Duc-dev-starter <leminhduck@gmail.com>
2024-08-07 11:14:55 +0700

    fix to deploy vercel

--- src/utils/localStorageHelper/index.ts
+++ src/utils/localStorageHelper/index.ts
@@ -1,4 +1,8 @@
+import { User } from "../../models";
 import { getCourseDetailByStudent } from "../../services";
+import { paths } from "../../consts";
+import { message } from "antd";
+import { useNavigate } from "react-router-dom";
 
 export const getCoursesFromLocalStorage = async () => {
     const courseIds = JSON.parse(localStorage.getItem("courseInWishList") || "[]");
@@ -6,3 +10,20 @@ export const getCoursesFromLocalStorage = async () => {
     const courseDetails = await Promise.all(getCourseDetail);
     return courseDetails;
   }
+
+  export const handSaveCourseToLocalStorage = (courseId: string, user: User,navigate: ReturnType<typeof useNavigate>) => {
+    if (!user) {
+        navigate(paths.LOGIN);
+        message.info('Please login before saving items to your wishlist.');
+    } else {
+        const courseInWishList = JSON.parse(localStorage.getItem("courseInWishList") || "[]");
+
+        if (courseInWishList.includes(courseId)) {
+            message.info("The course is already in the wishlist.");
+        } else {
+            courseInWishList.push(courseId);
+            localStorage.setItem("courseInWishList", JSON.stringify(courseInWishList));
+            message.success("Save Course To Wishlist Successfully!");
+        }
+    }
+};
